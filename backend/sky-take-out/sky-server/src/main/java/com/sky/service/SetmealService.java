78fc385940e501ecd0e66b80e1f6ca6322dfc7e5
KK <awsdkk33@gmail.com>
2026-01-19 22:30:12 +0800

    分页查询套餐业务代码开发

--- backend/sky-take-out/sky-server/src/main/java/com/sky/service/SetmealService.java
+++ backend/sky-take-out/sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -2,8 +2,10 @@ package com.sky.service;
 
 import com.sky.annotation.AutoFill;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
 
@@ -19,4 +21,11 @@ public interface SetmealService {
      */
     @AutoFill(OperationType.INSERT)
     void insert(Setmeal setmeal);
+
+    /**
+     * 套餐里的分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
