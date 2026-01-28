7f4820fdb487c58f5f3da78a3a92eb55f9839fd2
KK <awsdkk33@gmail.com>
2026-01-28 16:58:25 +0800

    工作台day12业务代码开发

--- backend/sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ backend/sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -89,4 +90,11 @@ public interface DishMapper {
      */
     @Select("select setmeal_id from setmeal_dish where dish_id = #{id}")
     String getSetmealId(Long id);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
